b1ed3e7517e8bcad64024c6a10048251e0c880f6
vin-ce <yc.li.vincent@gmail.com>
2023-08-01 15:13:25 -0700

    fix header stuff

--- components/layout.js
+++ components/layout.js
@@ -4,10 +4,6 @@ import { Open_Sans } from 'next/font/google'
 import Head from 'next/head'
 
 
-export const metadata = {
-  title: 'Circle',
-  description: 'Career',
-}
 
 export default function Layout({ children, className }) {
 
@@ -17,6 +13,15 @@ export default function Layout({ children, className }) {
         {/* prevents card going off the page from widening the page */}
         {/* https://stackoverflow.com/questions/24911220/how-to-prevent-mobile-device-to-resize-and-use-proper-css-file */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <title>Knock</title>
+        {/* <description>Explore your creative path</description> */}
       </Head>
       <div className={className}>
         <main>{children}</main>
